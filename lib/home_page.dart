6f462c26251642cd49cb15d963775208a14b4a6d
Aldi Riansyah <aldyrains30@gmail.com>
2024-06-28 17:04:35 +0700

    fix UI home page and grid view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/services.dart';
 import 'edit_contact_page.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -33,26 +35,34 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contacts'),
+        leading: IconButton(
+          icon: const Icon(Icons.search,color: Colors.orange,),
+          onPressed: () {},
+        ),
+        title: const Text(
+          'Contacts',
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
         actions: [
           IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add,color: Colors.orange,),
             onPressed: () {},
           ),
         ],
       ),
       body: RefreshIndicator(
         onRefresh: _pullRefresh,
-        child: ListView.builder(
+        child: GridView.builder(
+          padding: const EdgeInsets.all(10.0),
           itemCount: contacts.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+          ),
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${contacts[index]["firstName"]} ${contacts[index]["lastName"]}'),
-              subtitle: Text(contacts[index]["email"] ?? 'No Email'),
+            return GestureDetector(
               onTap: () async {
                 final updatedContact = await Navigator.push(
                   context,
@@ -74,6 +84,39 @@ class _HomePageState extends State<HomePage> {
                   });
                 }
               },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.orange,
+                        radius: 30,
+                        child: Text(contacts[index]["firstName"][0]),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        '${contacts[index]["firstName"]} ${contacts[index]["lastName"]}',
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             );
           },
         ),
